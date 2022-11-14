45a2ad9c390f2cde17d57165e4001fe7a72537ce
pilipb <philipblecher@hotmail.com>
2022-11-14 14:46:46 +0000

    group solutions works!

--- try_again.m
+++ try_again.m
@@ -3,10 +3,10 @@ clear, clc
 c = readmatrix("ProblemRankData.csv");
 
 
-[students, groups] = size(c);
+[students, groups] = size(c)
 total = students*groups;
 
-f = reshape(c,1,total);
+f = reshape(c',[],1);
 
 % Make A matrices:
 
@@ -30,8 +30,8 @@ end
 b1 = ones(1, students);
 
 % A for max and min group size
-minGroup = 3;
-maxGroup = 5;
+minGroup = 4;
+maxGroup = 6;
 
 A2 = eye(groups);
 A2 = repmat(A2, 1, students);
@@ -50,11 +50,11 @@ ub = ones(total, 1);
 
 % Arranging for intlinprog
 
-A = [-A2;A3];
-b = [ -b2_min'; b3_max'];
+A = [A2;-A3];
+b = [ b3_max'; -b2_min'];
 
 A_eq = A1;
-b_eq = b1;
+b_eq = b1';
 
 
 % objective = minimise the sum of x * c
@@ -66,20 +66,55 @@ b_eq = b1;
 
 
 
-[x, fval] = intlinprog(f, intcon, A, b, A_eq, b_eq,lb, ub);
-X = reshape(x,students,groups);
+[x, fval] = intlinprog(f, intcon, A, b, A_eq, b_eq,lb, ub)
+solutions = reshape(x, groups,students)'
 
-% check total assignments
-sum(sum(X))
+C = c;
+plotSolutions(solutions, C, minGroup, maxGroup)
+groups = assignNames(solutions, groups);
 
-group_totals = [];
-for i = 1:groups
-    group_totals(i) = sum(X(:,i));
+%% Andre code for plot:
+function groupings = assignNames(solutions, numGroups)
+groupings = struct;
+for col = 1:numGroups
+    students = find(solutions(:,col))
+    groupings.(['group' num2str(col)]) = students;
 end
-close all
-figure
-bar(groups, group_totals)
+end
 
+%% Graphical checks
+function plotSolutions(solutions, C, minSize, maxSize)
+
+numGroups = width(C);
+realSolutions = solutions .* C;
+labels = {'1st Choice', '2nd Choice', '3rd Choice', '4th Choice', ...
+    '5th Choice'};
+worstChoice = max(max(realSolutions));
+
+close all
+figure()
+
+xLabels = {};
+plottingData = [];
+for i = 1: numGroups
+    xLabels{i} = ['Group ' num2str(i)];
+    scores = zeros(1, worstChoice);
+
+    for j = 1:worstChoice
+
+        scores(1, j) = sum(realSolutions(:, i)==j);
+    end
+    plottingData = [plottingData; scores];
+end
+
+X = categorical(xLabels);
+X = reordercats(X,xLabels);
+bar(X, plottingData, 'stacked')
+title(['Group sizes: ' num2str(minSize) ' to ' num2str(maxSize)])
+ylabel('Number of students per group')
+legend(labels{1:worstChoice}, 'location', 'bestoutside')
+
+end
 
 
 
